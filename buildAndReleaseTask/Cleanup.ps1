f9093dc27f24f53499f46853ae112db0de752046
Tom Austin <Tom.Austin@streets-heaver.com>
2023-08-07 14:36:24 +0100

    fix bug

--- buildAndReleaseTask/Cleanup.ps1
+++ buildAndReleaseTask/Cleanup.ps1
@@ -1,31 +1,19 @@
-$filesToExclude = Get-VstsInput -Name ExcludeFiles
-
-$filesToExclude = $filesToExclude -replace "/", "\"
-
-if (![string]::IsNullOrEmpty($filesToExclude)) {
-    $excludedFiles = $filesToExclude -split "`r`n"
-
-    foreach ($excludedFile in $excludedFiles) {
-        Write-Host "Excluding $excludedFile"
-    }
+if ($Env:Build_SourcesDirectory -and (Test-Path -Path $Env:Build_SourcesDirectory)){
+    Write-Host "Deleting $Env:Build_SourcesDirectory"
+    Remove-Item $Env:Build_SourcesDirectory -Force -Recurse
 }
 
-if ($Env:Build_SourcesDirectory -and (Test-Path -Path $Env:Build_SourcesDirectory)) {
-    Write-Host "Deleting files in $Env:Build_SourcesDirectory"
-    Get-ChildItem -Path $Env:Build_SourcesDirectory -File -Recurse | Where-Object { $_.FullName -notmatch [Regex]::Escape(($excludedFiles -join '|')) } | Remove-Item -Force
+if ($Env:Build_ArtifactStagingDirectory -and (Test-Path -Path $Env:Build_ArtifactStagingDirectory)){
+    Write-Host "Deleting $Env:Build_ArtifactStagingDirectory"
+    Remove-Item $Env:Build_ArtifactStagingDirectory -Force -Recurse
 }
 
-if ($Env:Build_ArtifactStagingDirectory -and (Test-Path -Path $Env:Build_ArtifactStagingDirectory)) {
-    Write-Host "Deleting files in $Env:Build_ArtifactStagingDirectory"
-    Get-ChildItem -Path $Env:Build_ArtifactStagingDirectory -File -Recurse | Where-Object { $_.FullName -notmatch [Regex]::Escape(($excludedFiles -join '|')) } | Remove-Item -Force
+if ($Env:Common_TestResultsDirectory -and (Test-Path -Path $Env:Common_TestResultsDirectory)){
+    Write-Host "Deleting $Env:Common_TestResultsDirectory"
+    Remove-Item $Env:Common_TestResultsDirectory -Force -Recurse
 }
 
-if ($Env:Common_TestResultsDirectory -and (Test-Path -Path $Env:Common_TestResultsDirectory)) {
-    Write-Host "Deleting files in $Env:Common_TestResultsDirectory"
-    Get-ChildItem -Path $Env:Common_TestResultsDirectory -File -Recurse | Where-Object { $_.FullName -notmatch [Regex]::Escape(($excludedFiles -join '|')) } | Remove-Item -Force
-}
-
-if ($Env:Build_BinariesDirectory -and (Test-Path -Path $Env:Build_BinariesDirectory)) {
-    Write-Host "Deleting files in $Env:Build_BinariesDirectory"
-    Get-ChildItem -Path $Env:Build_BinariesDirectory -File -Recurse | Where-Object { $_.FullName -notmatch [Regex]::Escape(($excludedFiles -join '|')) } | Remove-Item -Force
+if ($Env:Build_BinariesDirectory -and (Test-Path -Path $Env:Build_BinariesDirectory)){
+    Write-Host "Deleting $Env:Build_BinariesDirectory"
+    Remove-Item $Env:Build_BinariesDirectory -Force -Recurse
 }
